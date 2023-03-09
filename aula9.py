84607a1b29c934b5b3081969736bc04237fd1e49
lthurler <leothurler@gmail.com>
2023-03-08 23:32:35 -0300

    fim da aula9

---
+++ aula9.py
@@ -0,0 +1,11 @@
+frase = "Curso em Vídeo Python"
+print(frase.replace('Python', 'Android'))
+print(frase[9:21])
+print(frase[:15])
+print(frase[:15:2])
+print(frase.find('Vídeo'))
+print(frase.split())
+
+dividido = frase.split()
+print(dividido[0])
+print(dividido[2][3])
