2e2b0ebd4cda29141675c3e1bf2d11126620cff4
lthurler <leothurler@gmail.com>
2023-03-23 11:45:40 -0300

    desafios aula17

--- desafio78.py
+++ desafio78.py
@@ -1,16 +1,31 @@
 lista_numeros = list()
-maior = 0
-menor = 0
-
-for index in range(0, 6):
+posicao_maior = list()
+posicao_menor = list()
+maior = menor = 0
+ 
+for cont in range(0, 6):
     lista_numeros.append(int(input('Digite um número: ')))
 
-    if index == 0:
-        maior = menor = lista_numeros[index]
+    if cont == 0:
+        maior = menor = lista_numeros[cont]        
     
-    if lista_numeros[index] > maior:
-        maior = lista_numeros[index]
+    if lista_numeros[cont] > maior:
+        maior = lista_numeros[cont]
+            
+    if lista_numeros[cont] < menor:
+        menor = lista_numeros[cont]
+        
+for index, valor in enumerate(lista_numeros):
+    if valor == maior:
+        posicao_maior.append(index)
     
-    if lista_numeros[index] < menor:
-        menor = lista_numeros[index]
-    
+    if valor == menor:
+        posicao_menor.append(index)
+
+print('')
+print(f'Você digitou os valores {lista_numeros}')
+print('')
+print(f'O maior valor foi {maior} na posição {posicao_maior}')
+print(f'O menor valor foi {menor} na posição {posicao_menor}')
+print('')
+ 
